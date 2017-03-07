e1d1f9ee19e3f63d6ae244c318226aace6880533
Andrea Haupt <hauptaj@gmail.com>
2017-03-07 09:47:48 -0500

    final commit

--- public/controller.js
+++ public/controller.js
@@ -1,12 +1,26 @@
 var app = angular.module('myMod');
 
-app.controller('myController', function($scope, $http) {
+app.controller('myController', function($scope, $interval, $http) {
 
   $http({
     method: 'GET',
     url: '/phrase'
   }).then(function successCallback(response) {
+    console.log(response.data);
     $scope.myPhrases = response.data;
   });
+  
+  $interval(function(){
+    $http({
+      method: 'GET',
+      url: '/phrase'
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      $scope.myPhrases = response.data;
+    });
+
+  }, 5000);
+
+
 
 });
